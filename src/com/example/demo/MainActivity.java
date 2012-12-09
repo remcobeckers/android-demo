e1e257c3f095353f2abdec00047d45751a4d86e2
Remco Beckers <remco.beckers@gmail.com>
2012-12-09 16:54:04 +0100

    Example of Intent for Settings in menu

--- src/com/example/demo/MainActivity.java
+++ src/com/example/demo/MainActivity.java
@@ -8,9 +8,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -32,7 +34,6 @@ public class MainActivity extends Activity {
 		searchField = (EditText) findViewById(R.id.search_text);
 		searchButton = (Button) findViewById(R.id.search_button);
 		tweetsList = (ListView) findViewById(R.id.tweets_list);
-		
 		searchButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -50,6 +51,16 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.menu_settings : Intent settings = new Intent(this, SettingsActivity.class);
+									  startActivity(settings);
+									  return true;
+		    default : return false;
+		}
 	}
 
 	private void searchTwitter(String query) {
