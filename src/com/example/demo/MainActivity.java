ac4885a2e3fe95a206a541d292f41fcd5cfdd245
Remco Beckers <remco.beckers@gmail.com>
2012-12-09 14:44:33 +0100

    Add code for handling input

--- src/com/example/demo/MainActivity.java
+++ src/com/example/demo/MainActivity.java
@@ -3,13 +3,37 @@ package com.example.demo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private EditText searchField;
+	private Button searchButton;
+	private ListView tweetsList;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		searchField = (EditText) findViewById(R.id.search_text);
+		searchButton = (Button) findViewById(R.id.search_button);
+		tweetsList = (ListView) findViewById(R.id.tweets_list);
+		
+		searchButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String query = searchField.getText().toString();
+				if (!query.isEmpty()) {
+					searchTwitter(query);
+				}
+			}
+
+		});
 	}
 
 	@Override
@@ -17,6 +41,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+
+	private void searchTwitter(String query) {
+		Toast.makeText(this, "Searching for "+query, Toast.LENGTH_SHORT).show();
 	}
 
 }
